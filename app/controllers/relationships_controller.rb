06db954707f60ef3cff09ce1791ca5903b157928
mizuki.kondou <mizukikuma0513@gmail.com>
2023-01-17 04:45:36 +0000

    curriculum　complete

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,6 @@
 class RelationshipsController < ApplicationController
+  before_action :require_user_logged_in
+
   def create
     user = User.find(params[:follow_id])
     current_user.follow(user)
@@ -6,10 +8,13 @@ class RelationshipsController < ApplicationController
     redirect_to user
   end
 
-  def 
+  def destroy
     user = User.find(params[:follow_id])
     current_user.unfollow(user)
     flash[:success] = 'ユーザのフォローを解除しました。'
     redirect_to user
   end
 end
+
+
+
